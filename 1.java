c3188f63dc67bb47984a667ef70ca13f992f6dff
samalaakshaya143 <164491174+samalaakshaya143@users.noreply.github.com>
2025-09-16 11:00:25 +0530

    Add files via upload

--- 1.java
+++ 1.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
-class  Factorial
+class  
 {
-	static int fact(int n){
-		int res=1;
-		for(int i=1;i<=n;i++){
-			res=res*i;
-		}
-		return res;
-	}
-
-
 	public static void main(String[] args) 
 	{
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		System.out.println(fact(n));
+		System.out.println("Hello World!");
 	}
 }
